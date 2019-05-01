79af766c4b418ac7bb8b54ffe95576e229b77511
muhammed <muhammedpak94@gmail.com>
2019-05-01 21:38:47 +0300

    second

---
+++ MediumAppMP.Core/DataAccess/EntityFramework/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using MediumAppMP.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MediumAppMP.Core.DataAccess.EntityFramework
+{
+    public interface IEntityRepository<T> where T : class, IBaseEntity<T>, new()
+    {
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+
+    }
+}
